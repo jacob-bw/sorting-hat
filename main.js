cb5f7cb8f91a8704905155d9e2da4ada1489c4f2
Jacob <wittenberg.jacob@gmail.com>
2019-10-14 00:51:14 -0500

    savepoint

--- main.js
+++ main.js
@@ -23,52 +23,65 @@ const houses = [
         attribute: 'brave',
         number: '3',
         crest: 'https://vignette.wikia.nocookie.net/harrypotter/images/b/b1/Gryffindor_ClearBG.png/revision/latest/scale-to-width-down/350?cb=20190222162949'
+     },
+     {
+         name: 'Expelled',
+         punishment: 'Please turn in your wand for destruction',
+         img: 'https://images.vexels.com/media/users/3/132003/isolated/preview/a05496ffbb01a7f9b84cd0e6d8e5188e-sad-pumpkin-emoticon-by-vexels.png'
      }
 ];
 
-console.log(houses);
-
 const printToDom = (toPrint, divId) => {
     document.getElementById(divId).innerHTML = toPrint
-}
-
+};
 
 const sortingHat = () => {
     let firstYearName = document.getElementById('name-zone');
     let student = firstYearName.value;
-    console.log(student);
     let randomNumber = Math.floor(Math.random()*4);
-    console.log(randomNumber);
     let domString = ''
     const studentCard = houses[randomNumber]
-    console.log(studentCard);
-//    domString += ` 
-//    <div class="card">
-//        <h2>Congratulations ${student}!</h2>
-//        <p> You are in ${studentCard.name} this means you are ${studentCard.attribute}.</p>
-//        <img src=${studentCard.crest} alt= '...' />
-//        <button id="expel-student">Expel</button>
-//    </div>
-//    `
+    
+    domString += `
+    <div class="row">
+    <div class="col-sm-6">
+        <div class="card text-center w-100">
+            <div class="card-body">
+                <img src=${studentCard.crest} alt= '...' height="250" width="250" />
+                <h5 class="card-title">Congratulations ${student}!</h5>
+                <p>You are in ${studentCard.name} this means you are ${studentCard.attribute}.</p>
+                <button id="expel" href="#" class="btn btn-dark">Expel</button>
+            </div>
+        </div>
+    </div>
+    </div>
+    `;
+
+printToDom(domString, 'student-zone')
+};
+
+const expelStudent = () => {
+    let expelledStudent = document.getElementById('h5');
+    let exStudent = expelledStudent.value;
+    console.log(exStudent);
+    let domString = ''
+    const exStudentCard = houses['4'];
+    console.log(exStudentCard);
         domString += `
         <div class="row">
         <div class="col-sm-6">
-            <div class="card w-100">
+            <div class="card text-center w-100">
                 <div class="card-body">
-                    <img src=${studentCard.crest} alt= '...' height="250" width="250" />
-                    <h5 class="card-title">Congratulations ${student}!</h5>
-                    <p>You are in ${studentCard.name} this means you are ${studentCard.attribute}.</p>
-                    <a id="expel" href="#" class="btn btn-dark">Expel</a>
+                    <img src=${exStudentCard.img} alt= '...' height="250" width="250" />
+                    <h5 class="card-title">We're very disappointed, ${exStudent}.</h5>
+                    <p>You have been ${exStudentCard.name}. ${exStudentCard.punishment}.</p>
                 </div>
             </div>
         </div>
         </div>
-        `
-printToDom(domString, 'student-zone')
-}
-
-//const expelStudent = () => {}
-
+        `;
+printToDom(domString, 'ex-student-zone')
+};
 
 document.getElementById('sorter').addEventListener('click', sortingHat);
-//document.getElementById('expel-student').addEventListener('click', expelStudent);
+document.getElementById('expel').addEventListener('click', expelStudent);
